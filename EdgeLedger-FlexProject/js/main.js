0287dacd4d0f78b841f768d33cd1652879a6b9bf
younusraza909 <younusraza909@gmail.com>
2020-03-01 14:01:05 +0500

    Add Smooth Scroll To Website

--- EdgeLedger-FlexProject/js/main.js
+++ EdgeLedger-FlexProject/js/main.js
@@ -1,15 +1,37 @@
 // Initialize And add The Map
 
-function initMap(){
-    // Your Location
-    const loc={lat:42.361145,lng:-71.057083}
-    // Centerd Map On Location
-    const map=new google.maps.Map(document.querySelector(".map")
-    ,{
-        zoom:14,
-        center :loc
-    })
-    // The marker positioned at location
-    const marker=new google.maps.Marker({position:loc,map:map
-    })
+function initMap() {
+  // Your Location
+  const loc = { lat: 42.361145, lng: -71.057083 };
+  // Centerd Map On Location
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
 }
+//STICKY MENUE BACKGROUND
+window.addEventListener("scroll",function(){
+    if(window.scrollY>150){
+        document.querySelector("#navbar").style.opacity=0.9;
+    }else{
+        document.querySelector('#navbar').style.opacity=1;
+    }
+})
+
+// SMOOTH SCROLLING
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html,body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
